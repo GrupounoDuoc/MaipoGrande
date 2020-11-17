2ecd99ee00318a5c3b9eb1a292bcfffd46abcc7a
Jason Leon <jasonleonmartinez@gmail.com>
2020-11-17 09:03:41 -0300

    cambias al catalogo
    cambios al catalogo en bootstrapt de momento

--- APP/MaipoGrande/resources/views/catalogo.blade.php
+++ APP/MaipoGrande/resources/views/catalogo.blade.php
@@ -1,6 +1,8 @@
+<!--
 @if(!isset($_SESSION))
 | {{ session_start() }}
 @endif
+-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,15 +11,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Catálogo</title>
-    <link rel="stylesheet" href="css/catalogo.css">
-    <link rel="stylesheet" href="css/registro.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed" rel="stylesheet">
     <link rel="stylesheet" href="iconos/style.css">
     <!-- PWA -->
     @laravelPWA
 </head>
-
+<!--
 <body>
     <header id="cabecera">
         <img src="imagenes/manzana.png" class="img-logo">
@@ -29,12 +29,10 @@
                 <li><a href="/">Inicio</a></li>
                 <li><a href="login">Entrar</a></li>
                 <li><a href="registro">Registrarse</a></li>
-                <li><a href="contacto">Contacto</a></li>
                 <li><span class="icon-search" id="buscador"></span></li>
 
                 @else
                 <li><a href="/">Inicio</a></li>
-                <li><a href="contacto">Contacto</a></li>
                 <li><span class="icon-search" id="buscador"></span></li>
                 <li class="li-perfilUsuario">
                     <img src="imagenes/usuario.png" class="img-usuario" id="img-perfil">
@@ -59,65 +57,144 @@
             @endif
         </ul>
     </div>
-    <form action="{{ route('catalogo') }}" method="POST">
-        @csrf
-        <div class="menu-lateral">
-            <nav class="submenu-lateral">
-                <ul class="lista-lateral">
-                    <li><span><img src="imagenes/icon-fruta.png"></span>Tipo fruta</li>
-                    <ul>
-                        @foreach ($tipos as $tipo)
-                        @if( $tipo->TIPO_FRUTA == $tipoSelected)
-                        <li><input type="radio" name="tipo" checked value="{{ $tipo->TIPO_FRUTA}}"><label for="{{ $tipo->TIPO_FRUTA}}">{{ $tipo->TIPO_FRUTA}}</label></li>
-                        @else
-                        <li><input type="radio" name="tipo" value="{{ $tipo->TIPO_FRUTA}}"><label for="{{ $tipo->TIPO_FRUTA}}">{{ $tipo->TIPO_FRUTA}}</label></li>
-                        @endif
-                        @endforeach
-                    </ul>
-                    <li><span><img src=""></span>Calidad</li>
-                    <ul>
-                        @foreach ($calidades as $calidad)
-                        @if( $calidad->CALIDAD == $calidadSelected)
-                        <li><input type="radio" name="calidad" checked value="{{ $calidad->CALIDAD}}"><label for="{{ $calidad->CALIDAD}}">{{ $calidad->CALIDAD}}</label></li>
-                        @else
-                        <li><input type="radio" name="calidad" value="{{ $calidad->CALIDAD}}"><label for="{{ $calidad->CALIDAD}}">{{ $calidad->CALIDAD}}</label></li>
-                        @endif
-                        @endforeach
-                    </ul>
-                    <br>
-                    <input type="submit" name="send" value="Filtrar">
-                    <button><a href="{{ action('App\Http\Controllers\pedidoController@catalogo') }}" class="button">Limpiar filtros </a></button>
-                </ul>
-            </nav>
+
+    <div class="row">
+        <div class="col-sm-4">  
+            <form action="{{ route('catalogo') }}" method="POST">
+                @csrf
+                <div class="menu-lateral">
+                    <nav class="submenu-lateral">
+                        <ul class="lista-lateral">
+                            <li><span><img src="imagenes/icon-fruta.png"></span>Tipo fruta</li>
+                            <ul>
+                                @foreach ($tipos as $tipo)
+                                @if( $tipo->TIPO_FRUTA == $tipoSelected)
+                                <li><input type="radio" name="tipo" checked value="{{ $tipo->TIPO_FRUTA}}"><label for="{{ $tipo->TIPO_FRUTA}}">{{ $tipo->TIPO_FRUTA}}</label></li>
+                                @else
+                                <li><input type="radio" name="tipo" value="{{ $tipo->TIPO_FRUTA}}"><label for="{{ $tipo->TIPO_FRUTA}}">{{ $tipo->TIPO_FRUTA}}</label></li>
+                                @endif
+                                @endforeach
+                            </ul>
+                            <li><span></span>Calidad</li>
+                            <ul>
+                                @foreach ($calidades as $calidad)
+                                @if( $calidad->CALIDAD == $calidadSelected)
+                                <li><input type="radio" name="calidad" checked value="{{ $calidad->CALIDAD}}"><label for="{{ $calidad->CALIDAD}}">{{ $calidad->CALIDAD}}</label></li>
+                                @else
+                                <li><input type="radio" name="calidad" value="{{ $calidad->CALIDAD}}"><label for="{{ $calidad->CALIDAD}}">{{ $calidad->CALIDAD}}</label></li>
+                                @endif
+                                @endforeach
+                            </ul>
+                            <br>
+                            <input type="submit" class="btn btn-success" name="send" value="Filtrar">
+                            <button><a href="{{ action('App\Http\Controllers\pedidoController@catalogo') }}" class="btn btn-primary">Limpiar filtros </a></button>
+                        </ul>
+                    </nav>
+                </div>
+            </form>
         </div>
-    </form>
-    <div class="contenido-productos">
-        @foreach ($ofertas as $oferta)
-        <div class="card" style="width: 18rem;">
-            <img src="data:image/png;base64,{{ chunk_split(base64_encode($oferta->FOTO)) }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $oferta->TIPO_FRUTA}}</h5>
-                <p class="card-text">Vendedor: {{ $oferta->NOMBRE_VENDEDOR}}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Tipo de calidad: {{ $oferta->CALIDAD}}</li>
-                <li class="list-group-item">Precio: $ {{ $oferta->PRECIO}} {{ $oferta->MONEDA }}</li>
-            </ul>
-            <div class="centrar-texto">
-                <a href="#" class="btn btn-outline-success">Ver más</button>
-                    <a class="btn btn-outline-success" href="addCart/{{ $oferta->ID}}" role="button">Añadir al carro</a>
+        <div class="col-sm-8">
+            <div class="contenido-productos">
+                @foreach ($ofertas as $oferta)
+                <div class="card">
+                    <img src="data:image/png;base64,{{ chunk_split(base64_encode($oferta->FOTO)) }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $oferta->TIPO_FRUTA}}</h5>
+                        <p class="card-text">Vendedor: {{ $oferta->NOMBRE_VENDEDOR}}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Tipo de calidad: {{ $oferta->CALIDAD}}</li>
+                        <li class="list-group-item">Precio: $ {{ $oferta->PRECIO}} {{ $oferta->MONEDA }}</li>
+                    </ul>
+                    <div class="centrar-texto">
+                        <a href="#" class="btn btn-info">Ver más</button>
+                            <a class="btn btn-danger" href="addCart/{{ $oferta->ID}}" role="button">Añadir al carro</a>
+                    </div>
+            
+                </div>
+                @endforeach
             </div>
         </div>
-        @endforeach
     </div>
+
+-->
+
+    <body>
+
+        
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <img src="imagenes/manzana.png" style="height:1.25rem; margin-right:0.8rem">
+            <a class="navbar-brand" href="/">Maipo Grande</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="catalogo"> Catalogo <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registro">Registrarse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="maipogrande">Calidad Fruta</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+
+        <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-3 col-xl-2 bg-primary" >
+                <h1>hola</h1>
+            </div>
+            <div class="col-sm-12 col-md-8 col-lg-9 col-xl-10 bg-danger">
+                <div class="row" style="padding:20px">
+                    <div class="col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                        @foreach ($ofertas as $oferta)
+                            <div class="card" style="width: 100%">
+                                <img src="data:image/png;base64,{{ chunk_split(base64_encode($oferta->FOTO)) }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $oferta->TIPO_FRUTA}}</h5>
+                                    <p class="card-text">Vendedor: {{ $oferta->NOMBRE_VENDEDOR}}</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Tipo de calidad: {{ $oferta->CALIDAD}}</li>
+                                    <li class="list-group-item">Precio: $ {{ $oferta->PRECIO}} {{ $oferta->MONEDA }}</li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="addCart/{{ $oferta->ID}}" class="card-link">Añadir al carro</a>
+                                    <a href="#" class="card-link">Ver mas</a>
+                                </div>
+                            </div>    
+                        @endforeach
+                    </div>    
+                </div>    
+            </div>
+        </div>
+
+        <footer class="page-footer font-small blue">
+            <div class="footer-copyright text-center py-3">
+                © 2020 Todos los derechos reservados | Diseñado por <a href="/"> Maipo Grande</a>
+            </div>
+        </footer>
+
     <script>
         if (window.history.replaceState) {
             window.history.replaceState(null, null, window.location.href);
         }
     </script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
     <script src="js/buscar.js"></script>
     <script src="js/rangoPrecios.js"></script>
     <script src="js/aparecerIcono.js"></script>
