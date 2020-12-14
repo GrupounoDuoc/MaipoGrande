c9808c03feef42790dd69f4804349589e35833de
Jason Leon <jasonleonmartinez@gmail.com>
2020-12-14 18:45:45 -0300

    postular pedidos logisticas
    uwu

--- APP/MaipoGrande/resources/views/PedidosLogistica.blade.php
+++ APP/MaipoGrande/resources/views/PedidosLogistica.blade.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION)) {
 <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&display=swap" rel="stylesheet">
 
 
-
+<link rel="stylesheet" href="css/logistica.css">
 <!-- Font Awesome Icons -->
 <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
 <!-- Theme style -->
@@ -42,25 +42,82 @@ if (!isset($_SESSION)) {
     </div>
 </nav>
 
-<div class="contenedor">
-    <br>
-    <h2>Pedidos en logística</h2>
-    <br>
+@if(Session::has('message'))
+<div class="alert alert-{{ Session::get('type') }} alert-dismissable fade show text-center" role="alert">
+    {{ Session::get('message') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
 
-    @if(Session::has('message'))
-    <div class="alert alert-{{ Session::get('type') }} alert-dismissable fade show text-center" role="alert">
-        {{ Session::get('message') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
+<div class="container">
+    <div class="container my-5">
+        <br>
+        <h2>Pedidos en logística</h2>
+        <hr>
+        <form action="postulartr" id="postulartransporteform" method="POST">
+            @csrf
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Id Pedido</th>
+                            <th scope="col">Correo Solicitante</th>
+                            <th scope="col">Medio de transporte</th>
+                            <th scope="col">Refrigeración</th>
+                            <th scope="col">Peso de carga</th>
+                            <th scope="col">Acción</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <div class="container">
+                            @foreach($Disponibles as $key => $VentasDisponibles)
+                            <tr>
+                                <td>{{$VentasDisponibles->ID_PEDIDO}}</td>
+                                <td>{{$VentasDisponibles->CORREO_COMPRADOR}}</td>
+                                <td>{{$VentasDisponibles->METODO_VIAJE}}</td>
+                                <td>
+                                    @if($VentasDisponibles->REFRIGERADO == 0)
+                                    No
+                                    @elseif($VentasDisponibles->REFRIGERADO == 1)
+                                    Si
+                                    @endif
+                                </td>
+                                <td>{{$VentasDisponibles->CANT_KG_TOTAL}}</td>
+                                <td>
+                                    <input class="btn btn-success my-2 my-sm-0" type="submit" name="Postular{{$VentasDisponibles->ID_PEDIDO}}" value="Postular">
+                                </td>
+                            </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </form>
     </div>
-    @endif
 
 
-    <form action="postulartr" id="postulartransporteform" method="POST">
-        @csrf
-        <div>
-            <table class="table">
+
+
+
+
+    <div class="container my-5">
+        <br>
+        <h2>Postulaciones</h2>
+        <hr>
+
+        @if(Session::has('message'))
+        <div class="alert alert-{{ Session::get('type') }} alert-dismissable fade show text-center" role="alert">
+            {{ Session::get('message') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
+
+
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
                 <thead class="thead-dark">
                     <tr>
                         <th scope="col">Id Pedido</th>
@@ -68,84 +125,31 @@ if (!isset($_SESSION)) {
                         <th scope="col">Medio de transporte</th>
                         <th scope="col">Refrigeración</th>
                         <th scope="col">Peso de carga</th>
-                        <th scope="col">Acción</th>
+                        <th scope="col">Estado del pedido</th>
                     </tr>
                 </thead>
                 <tbody>
                     <div class="container">
-                        @foreach($Disponibles as $key => $VentasDisponibles)
+                        @foreach($Postulaciones as $key => $Postulados)
                         <tr>
-                            <td>{{$VentasDisponibles->ID_PEDIDO}}</td>
-                            <td>{{$VentasDisponibles->CORREO_COMPRADOR}}</td>
-                            <td>{{$VentasDisponibles->METODO_VIAJE}}</td>
+                            <td>{{$Postulados->ID_PEDIDO}}</td>
+                            <td>{{$Postulados->CORREO_COMPRADOR}}</td>
+                            <td>{{$Postulados->METODO_VIAJE}}</td>
                             <td>
-                                @if($VentasDisponibles->REFRIGERADO == 0)
+                                @if($Postulados->REFRIGERADO == 0)
                                 No
-                                @elseif($VentasDisponibles->REFRIGERADO == 1)
+                                @elseif($Postulados->REFRIGERADO == 1)
                                 Si
                                 @endif
                             </td>
-                            <td>{{$VentasDisponibles->CANT_KG_TOTAL}}</td>
-                            <td>
-                                <input class="btn btn-success my-2 my-sm-0" type="submit" name="Postular{{$VentasDisponibles->ID_PEDIDO}}" value="Postular">
-                            </td>
+                            <td>{{$Postulados->CANT_KG_TOTAL}}</td>
+                            <td>{{$Postulados->ESTADO}}</td>
                         </tr>
                         @endforeach
                 </tbody>
             </table>
         </div>
-    </form>
-</div>
-
-<div class="contenedor">
-    <br>
-    <h2>Postulaciones</h2>
-    <br>
-
-    @if(Session::has('message'))
-    <div class="alert alert-{{ Session::get('type') }} alert-dismissable fade show text-center" role="alert">
-        {{ Session::get('message') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
     </div>
-    @endif
-
-
-    <div>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Id Pedido</th>
-                    <th scope="col">Correo Solicitante</th>
-                    <th scope="col">Medio de transporte</th>
-                    <th scope="col">Refrigeración</th>
-                    <th scope="col">Peso de carga</th>
-                    <th scope="col">Estado del pedido</th>
-                </tr>
-            </thead>
-            <tbody>
-                <div class="container">
-                    @foreach($Postulaciones as $key => $Postulados)
-                    <tr>
-                        <td>{{$Postulados->ID_PEDIDO}}</td>
-                        <td>{{$Postulados->CORREO_COMPRADOR}}</td>
-                        <td>{{$Postulados->METODO_VIAJE}}</td>
-                        <td>
-                            @if($Postulados->REFRIGERADO == 0)
-                            No
-                            @elseif($Postulados->REFRIGERADO == 1)
-                            Si
-                            @endif
-                        </td>
-                        <td>{{$Postulados->CANT_KG_TOTAL}}</td>
-                        <td>{{$Postulados->ESTADO}}</td>
-                    </tr>
-                    @endforeach
-            </tbody>
-        </table>
-    </div>
-</div>
 
 
 
@@ -154,74 +158,67 @@ if (!isset($_SESSION)) {
 
 
 
-<div class="contenedor">
-    <br>
-    <h2>Pedidos históricos</h2>
-    <br>
+    <div class="container my-5">
+        <br>
+        <h2>Pedidos históricos</h2>
+        <hr>
 
-    @if(Session::has('message'))
-    <div class="alert alert-{{ Session::get('type') }} alert-dismissable fade show text-center" role="alert">
-        {{ Session::get('message') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    @endif
-
-
-
-    <form action="{{ route('ActualizarEstado') }}" id="actualizarForm" method="POST">
-        @csrf
-        <div>
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Id Pedido</th>
-                        <th scope="col">Correo Solicitante</th>
-                        <th scope="col">Medio de transporte</th>
-                        <th scope="col">Refrigeración</th>
-                        <th scope="col">Peso de carga</th>
-                        <th scope="col">Acción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <div class="container">
-                        @foreach($Historicos as $key => $vHistorico)
-                        <tr>
-                            <td>{{$vHistorico->ID_PEDIDO}}</td>
-                            <td>{{$vHistorico->CORREO_COMPRADOR}}</td>
-                            <td>{{$vHistorico->METODO_VIAJE}}</td>
-                            <td>
-                                @if($vHistorico->REFRIGERADO == 0)
-                                No
-                                @elseif($vHistorico->REFRIGERADO == 1)
-                                Si
-                                @endif
-                            </td>
-                            <td>{{$vHistorico->CANT_KG_TOTAL}}</td>
-                            <td>{{$vHistorico->ESTADO}}</td>
-                            <td>
-                                Actualizar Estado
-                            </td>
-                        </tr>
-                        @endforeach
-                </tbody>
-            </table>
+        @if(Session::has('message'))
+        <div class="alert alert-{{ Session::get('type') }} alert-dismissable fade show text-center" role="alert">
+            {{ Session::get('message') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-    </form>
+        @endif
 
+
+
+        <form action="{{ route('ActualizarEstado') }}" id="actualizarForm" method="POST">
+            @csrf
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Id Pedido</th>
+                            <th scope="col">Correo Solicitante</th>
+                            <th scope="col">Medio de transporte</th>
+                            <th scope="col">Refrigeración</th>
+                            <th scope="col">Peso de carga</th>
+                            <th scope="col">Acción</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <div class="container">
+                            @foreach($Historicos as $key => $vHistorico)
+                            <tr>
+                                <td>{{$vHistorico->ID_PEDIDO}}</td>
+                                <td>{{$vHistorico->CORREO_COMPRADOR}}</td>
+                                <td>{{$vHistorico->METODO_VIAJE}}</td>
+                                <td>
+                                    @if($vHistorico->REFRIGERADO == 0)
+                                    No
+                                    @elseif($vHistorico->REFRIGERADO == 1)
+                                    Si
+                                    @endif
+                                </td>
+                                <td>{{$vHistorico->CANT_KG_TOTAL}}</td>
+                                <td>{{$vHistorico->ESTADO}}</td>
+                                <td>
+                                    Actualizar Estado
+                                </td>
+                            </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </form>
+
+    </div>
+    <a href="pedidos" class="btn btn-danger col-sm-12 col-xs-12 col-md-2 btn-form  mx-2 my-5"><i class="fas fa-backspace"></i> Volver</a>
 </div>
 
 
 
 
 <!--Footer-->
-<footer>
-    <div class="contenedor">
-        <div class="d-flex p-2 justify-content-center">
-            <div class="copyright">
-                © 2020 Todos los derechos reservados | Diseñado por <a href="/"> Maipo Grande </a>
-            </div>
-        </div>
-    </div>
-</footer>
